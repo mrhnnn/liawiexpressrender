f60723d41a07e7947fb2f739da7b34f9a2b53c99
Felix Liawi <felixliawi@gmail.com>
2022-09-07 12:09:48 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello world"));
+app.get("/ping", (req, res) => res.send("Pong"));
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
