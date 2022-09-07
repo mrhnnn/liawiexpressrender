c28985d5a4a4f29006ede10df50358c7e8a58ad2
Felix Liawi <felixliawi@gmail.com>
2022-09-07 12:17:40 +0800

    Add new endpoint

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello world"));
 app.get("/ping", (req, res) => res.send("Pong"));
+app.get("/felix", (req, res) => res.send("Liawi"));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
