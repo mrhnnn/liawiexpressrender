3ae8bf4876c74e1764a173de959c29989ab963f9
mrhnnn <mraihan030211@gmail.com>
2023-05-19 10:45:09 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello world"));
-app.get("/ping", (req, res) => res.send("Pong"));
-app.get("/felix", (req, res) => res.send("Liawi"));
+app.get("/express", (req, res) => res.send("Express"));
+app.get("/example", (req, res) => res.send("Example"));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
